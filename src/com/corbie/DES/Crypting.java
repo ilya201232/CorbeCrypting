9993ac254ac8d869e7bf6556696ae073c7bd7520
ilya201232 <44816360+ilya201232@users.noreply.github.com>
2019-03-21 14:59:29 +0300

    Добавил интерфейс с командами для работы с пользователем

--- src/com/corbie/DES/Crypting.java
+++ src/com/corbie/DES/Crypting.java
@@ -3,24 +3,63 @@ package com.corbie.DES;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.util.Scanner;
 
 public class Crypting {
 
-    public static void main(String[] argv){
-        try {
-            SecretKey key = KeyGenerator.getInstance("DES").generateKey();
-            DesEncrypter encrypter = new DesEncrypter(key);
-            String encrypted = encrypter.encrypt("ЛОЛ а ты прикольный");
-            String decrypted = encrypter.decrypt(encrypted);
+    static void chat(String msg, SecretKey key) throws Exception{
+        DesEncrypter encrypter = new DesEncrypter(key);
+        String encrypted = encrypter.encrypt(msg);
+        System.out.println("Зашифрованное сообщение >>> " + encrypted);
+        String decrypted = encrypter.decrypt(encrypted);
+        System.out.println("Расшифрованное сообщение >>> " + decrypted);
+    }
 
-            System.out.println(encrypted + " ___________________ " + decrypted);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
+    public static void main(String[] argv) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        SecretKey key = KeyGenerator.getInstance("DES").generateKey();
 
 
+        System.out.print("Добро пожаловать в альфа версию мессенждера Corbie!\n" +
+                "Для помощи введите help\n" +
+                "Введите команду:\n");
 
+        String com;
+        boolean f = false;
+        do{
+            com = sc.next();
+            switch (com) {
+                case "help":
+                    System.out.print("Возможные команды:\n" +
+                            "help - вывод справки\n" +
+                            "enter_msg - Ввод сообщения\n" +
+                            "getSecKey - Вывод ключа шифрования\n" +
+                            "getAlName - Вывод названия алгоритма\n" +
+                            "EXIT - Выход\n");
+                    break;
+                case "enter_msg":
+                    System.out.println("Введите сообщение:");
+                    sc.useDelimiter("\n");
+                    String msg = sc.next();
+                    chat(msg, key);
+                    f = true;
+                    break;
+                case "getSecKey":
+                    System.out.println("Ваш ключ шифрования: " + key);
+                    f = true;
+                    break;
+                case "getAlName":
+                    System.out.println("DES");
+                    f = true;
+                    break;
+                case "EXIT":
+                    System.out.println("Пока!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Неверная команда. Повторите попытку");
+                    break;
+            }
+        }while (f == false);
     }
 }
